95fd3693739f634ba6947b7c7d9315a11f54560c
Carlos Gonzalez <caal.0522@gmail.com>
2022-03-07 17:05:58 -0500

    Adds complete example for Pane Switcher.

--- src/PaneSwitcherExample/PaneSwitcherExample.tsx
+++ src/PaneSwitcherExample/PaneSwitcherExample.tsx
@@ -1,12 +1,28 @@
 import { PaneSwitcher, Pane } from '../PaneSwitcher'
 import { PANE_IDS } from './constants'
 import Main from './PaneContents/Main'
+import Submenu from './PaneContents/Submenu'
+import Subsubmenu from './PaneContents/Subsubmenu'
+import Other from './PaneContents/Other'
+import SubOther from './PaneContents/SubOther'
 
 const PaneSwitcherExample = () => {
   return (
     <PaneSwitcher defaultPaneId={PANE_IDS.main}>
       <Pane paneId={PANE_IDS.main}>
         <Main />
+      </Pane>
+      <Pane paneId={PANE_IDS.submain}>
+        <Submenu />
+      </Pane>
+      <Pane paneId={PANE_IDS.subsubmain}>
+        <Subsubmenu />
+      </Pane>
+      <Pane paneId={PANE_IDS.other}>
+        <Other />
+      </Pane>
+      <Pane paneId={PANE_IDS.subother}>
+        <SubOther />
       </Pane>
     </PaneSwitcher>
   )
