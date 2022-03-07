1a0f125dc5ad5a4cfb5e674b2aa11ced6b878049
Carlos Gonzalez <caal.0522@gmail.com>
2022-03-07 17:10:00 -0500

    Adds some text explaining the purpose of this repo.

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,14 @@ import PaneSwitcherExample from "./PaneSwitcherExample";
 function App() {
   return (
     <>
-    <h1>Hello!</h1>
-    <PaneSwitcherExample />
-   </>
+      <h1>Hello!</h1>
+      <p>
+        This repo contains a small example of a <strong>pane switcher</strong>, it only shows the selected Pane,
+        and children can decide which pane should be set as selected, this is useful for complex navigation scenarios
+        or for complex wizard flows. the following is an example:
+      </p>
+      <PaneSwitcherExample />
+    </>
   );
 }
 
